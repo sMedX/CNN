e60ef6d7990b37a462360d768bc91e5143e81d1c
Korabelnikov A <nesoriti@yandex.ru>
2016-02-19 16:15:47 +0400

    utility for generation binary mask. useful for cutting

---
+++ validation/createRectangleMaskAroundMask.cpp
@@ -0,0 +1,89 @@
+///
+/// Cuts image by tiles (by the corresponded mask) and saves it in folders positive/ and negative/. Adds file list text file.
+/// If mask provided then it used as negative. Else use bounding box as negative.
+///
+#include <iostream>
+#include <fstream>
+#include <omp.h>
+
+#include <itkImage.h>
+#include <itkMetaImageIOFactory.h>
+#include <itkNrrdImageIOFactory.h>
+
+#include "agtkTypes.h"
+#include "agtkIO.h"
+#include "agtkCommandLineArgumentParser.h"
+#include "agtkBinaryImageUtilities.h"
+
+int main(int argc, char* argv[])
+{
+  using namespace agtk;
+
+  itk::MetaImageIOFactory::RegisterOneFactory();
+  itk::NrrdImageIOFactory::RegisterOneFactory();
+  std::cout << "nrrd factory registered" << std::endl;
+
+  typedef std::list<itk::LightObject::Pointer> RegisteredObjectsContainerType;
+  RegisteredObjectsContainerType registeredIOs = itk::ObjectFactoryBase::CreateAllInstance("itkImageIOBase");
+  std::cout << "there are " << registeredIOs.size() << " IO objects available to the ImageFileReader." << std::endl;
+
+  /** Create a command line argument parser. */
+  CommandLineArgumentParser::Pointer parser = CommandLineArgumentParser::New();
+  parser->SetCommandLineArguments(argc, argv);
+
+  std::string maskName;
+  parser->GetValue("-maskName", maskName);
+
+  std::string listFile;
+  parser->GetValue("-listFile", listFile); // conains pathes without slashes on the end
+
+  int radius; // for padding
+  parser->GetValue("-radius", radius);
+
+  std::cout << "list file  " << listFile << std::endl;
+  std::cout << "maskName  " << maskName << std::endl;
+  std::cout << "radius " << radius << std::endl;
+
+  std::vector<std::string> inputDirs;
+
+  ifstream infile(listFile);
+  std::string line;
+
+  while (std::getline(infile, line)) {
+    inputDirs.push_back(line);
+  }
+  std::cout << "inputData.size() " << inputDirs.size() << std::endl;
+
+  itk::MultiThreader::SetGlobalMaximumNumberOfThreads(1);
+  omp_set_num_threads(24); // Use 24 threads
+
+#pragma omp parallel for
+  for (int iImage = 0; iImage < inputDirs.size(); ++iImage) {
+    auto inputDir = inputDirs[iImage];
+    auto maskFile = inputDir + "\\" + maskName;
+    auto outputFile = inputDir + "\\" + maskName + "-boundingRec-r" + std::to_string(radius) + ".nrrd";
+
+    std::cout << "maskFile " << maskFile << std::endl;
+
+    // read images
+    BinaryImage3D::Pointer mask = BinaryImage3D::New();
+    std::cout << "load mask" << std::endl;
+    if (!readImage(mask, maskFile)) {
+      std::cout << "can't read " << maskFile << std::endl;
+      continue;
+    }
+
+    auto rec = agtk::getBinaryMaskBoundingBoxRegion(mask);
+    rec.PadByRadius(radius);
+    rec.Crop(mask->GetLargestPossibleRegion());
+
+    mask->SetBufferedRegion(rec);
+    mask->FillBuffer(1);
+
+    mask->SetBufferedRegion(mask->GetLargestPossibleRegion());
+
+    writeImage(mask.GetPointer(), outputFile);
+
+  }
+  return EXIT_SUCCESS;
+};
