fc526d574ae10e284b8a3eec083b58de6d9f3bed
Korabelnikov A <nesoriti@yandex.ru>
2016-02-29 22:53:57 +0400

    minor fixes

--- py/RUNALL-CV.py
+++ py/RUNALL-CV.py
@@ -3,15 +3,15 @@ import subprocess
 from subprocess import call
 import errno
 
-classCount = '2'
+classCount = '3'
 cut = 'D:\\alex\\CNN-build\\utils\\Release\\CutImageByTiles.exe'
 makeTileLists = ['python.exe', 'make_sample_names_' + classCount + '-classes.py']
 clas = 'D:\\alex\\nets_pancreas\\caffe-win-1029\\bin\\class_new.exe'
 postproc = 'D:\\alex\\CNN-build\\postprocessing\\Release\\postprocessing.exe'
 valid = 'D:\\alex\\CNN-build\\validation\\Release\\Validation.exe'
 
-preset = 'liver'
-ver = '2_'
+preset = 'pancreas'
+ver = '6__'
 dir=os.path.join('C:/caffe', preset, ver)
 print dir
 
@@ -47,26 +47,26 @@ def main():
         if e.errno != errno.EEXIST:
             raise e
         pass
-    deviceId = '1'
+    deviceId = '0'
     iters = '80000'
 
     n = 4  # number of parts for cross-validation
 
     for k in range(0, n):
         kn = str(k) + '-' + str(n)
-        sampleTrainListK = os.path.join(dir, 'tileList-train-cv-' + kn + '.txt')
-        sampleTestListK = os.path.join(dir, 'tileList-test-cv-' + kn + '.txt')
+        sampleTrainListK = os.path.join(dir, 'train-cv-' + kn + '.txt')
+        sampleTestListK = os.path.join(dir, 'test-cv-' + kn + '.txt')
 
-        tileTrainListK = os.path.join(dir, 'train-cv-' + kn + '.txt')
-        tileTestListK = os.path.join(dir, 'test-cv-' + kn + '.txt')
+        tileTrainListK = os.path.join(dir, 'tileList-train-cv-' + kn + '.txt')
+        tileTestListK = os.path.join(dir, 'tileList-test-cv-' + kn + '.txt')
 
         solver = createNetAndSolver(kn, tileTestListK, tileTrainListK,iters, snapshotFolder)
 
-        if (k!=0):
-            subprocess.call([makeTileLists[0], makeTileLists[1], tilesFolder, dir, str(k), str(n)])
+        subprocess.call([makeTileLists[0], makeTileLists[1], tilesFolder, dir, str(k), str(n)])
 
         train = 'C:\\caffe\\bin\\caffe_cc35.exe'
-        subprocess.call([train, 'train', '--solver', solver, '--gpu', deviceId])
+        if (k!=0):
+            subprocess.call([train, 'train', '--solver', solver, '--gpu', deviceId])
 
         deploy = os.path.join('C:\\caffe', preset, ver, 'deploy.prototxt')
         model = os.path.join('D:/artem/caffe/snap', preset, ver, '_iter_' + iters + '.caffemodel')
@@ -94,6 +94,7 @@ def main():
 
         with open(samplesList) as f:
             for line in f:
+                line = line[:-1]
                 subprocess.call([postproc, '-image', os.path.join(line, outputCNN), '-gaussianVariance', sigma])
 
         suffix = suffix + '-gaussian' + sigma + '.nrrd'
@@ -106,10 +107,12 @@ def main():
 def validate(samplesTrainListK, samplesTestListK, label, outputCNN, suffix):
     with open(samplesTrainListK) as f:
         for line in f:
+            line = line[:-1]
             subprocess.call([valid, '-testImage', os.path.join(line, outputCNN), '-label', os.path.join(line, label),
                              '-report report-cnn-train-' + suffix + '.csv'])
     with open(samplesTestListK) as f:
         for line in f:
+            line = line[:-1]
             subprocess.call([valid, '-testImage', os.path.join(line, outputCNN), '-label', os.path.join(line, label),
                              '-report report-cnn-test-' + suffix + '.csv'])
     return valid
