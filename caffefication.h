c5d879cb725fe996fc9164581a9c25001feb772b
Korabelnikov A <nesoriti@yandex.ru>
2016-05-13 23:39:50 +0400

    fix for linux

--- caffefication.h
+++ caffefication.h
@@ -1,7 +1,9 @@
-#ifdef caffefication_EXPORTS
-#  define CAFFEFICATION_EXPORT  __declspec(dllexport)
-#else
-#  define CAFFEFICATION_EXPORT
+#ifdef _WIN32
+#  ifdef caffefication_EXPORTS
+#    define CAFFEFICATION_EXPORT  __declspec(dllexport)
+#  else
+#    define CAFFEFICATION_EXPORT
+#  endif
 #endif
 
 #define DECLARE_DLL_FUNCTION(ret, name, params)\
@@ -15,17 +17,17 @@
 // Forward declaration
 namespace caffe
 {
-template <typename T>
-class Net;
+  template <typename T>
+  class Net;
 }
 
 namespace caffefication
 {
-using namespace agtk;
+  using namespace agtk;
 
-extern "C" DECLARE_DLL_FUNCTION(bool, classify, (caffe::Net<float>* caffeNet, const std::string& preset, Int16Image3D::Pointer image16,
-  UInt8Image3D::Pointer imageMask, Image3DRegion& region, int radiusXY, float spacingXY, int batchLength, int groupX,
-  int groupY, int classCount, bool isRgb, OUT BinaryImage3D::Pointer& outImage))
+  extern "C" DECLARE_DLL_FUNCTION(bool, classify, (caffe::Net<float>* caffeNet, const std::string& preset, Int16Image3D::Pointer image16,
+    UInt8Image3D::Pointer imageMask, Image3DRegion& region, int radiusXY, float spacingXY, int batchLength, int groupX,
+    int groupY, int classCount, bool isRgb, /*OUT*/ BinaryImage3D::Pointer& outImage))
 
-extern "C" DECLARE_DLL_FUNCTION(void, loadNet, (const std::string& modelFile, const std::string& trainedFile, int deviceId, std::shared_ptr<caffe::Net<float>>& caffeNet))
+    extern "C" DECLARE_DLL_FUNCTION(void, loadNet, (const std::string& modelFile, const std::string& trainedFile, int deviceId, std::shared_ptr<caffe::Net<float>>& caffeNet))
 }
