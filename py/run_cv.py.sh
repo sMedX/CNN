1effd476f4f271a56b176fbb66683335439e40fc
Korabelnikov A <nesoriti@yandex.ru>
2016-09-26 21:28:50 +0400

    shell script for linux

---
+++ py/run_cv.py.sh
@@ -0,0 +1,16 @@
+#export PATH=$PATH:/home/himanshu/practice/
+
+#todo
+export cut=/root/host/CNN-utils-build/cutImageByTiles
+export caffe=
+export clas=
+export postproc=
+export valid=
+export python=/bin/python
+
+export nets=/root/host/
+export images=/root/host/data
+export tiles=/root/host/data
+export snaps=/root/host/
+
+$python run_cv.py
