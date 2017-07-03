9d0cead6e861f1bf62f9efb79f5009a5b2ec1611
Manvel Avetisian <avetisian@gmail.com>
2017-07-03 09:31:26 +0300

    Added a code to shard calculations to multiple processes.

--- ultrasound/clusterize_videos.py
+++ ultrasound/clusterize_videos.py
@@ -5,11 +5,14 @@ import gflags
 import numpy as np
 import pickle
 from datetime import datetime
+from collections import defaultdict
 import sklearn.cluster
 import scipy.spatial
 
 FLAGS = gflags.FLAGS
 gflags.DEFINE_string("vectors", "vectors.pickle", "")
+gflags.DEFINE_integer("shard", 0, "")
+gflags.DEFINE_integer("num_shards", 4, "")
 FLAGS(sys.argv)
 
 filename_to_video = lambda s: os.path.basename(s).split("_")[0]
@@ -32,11 +35,13 @@ indexed_keys = np.array([key_indices[k] for k in key_filenames])
 print("building trees")
 trees = [scipy.spatial.KDTree(X[indexed_keys == k]) for k in range(len(key_indices))]
 
-distance_matrix = np.zeros((len(key_indices), len(key_indices)))
+distances = defaultdict(list)
 
 start_time = datetime.now()
 
 for i, x in enumerate(X):
+    if i % FLAGS.num_shards != FLAGS.shard: continue
+
     time_passed = float((datetime.now() - start_time).total_seconds())
     eta = (time_passed / (i + 1)) * (X.shape[0] - i) / 60 / 60
     print("%8d\t%3.2f%%\teta = %.2f hours" % (i, 100. * i / float(X.shape[0]), eta))
@@ -46,7 +51,6 @@ for i, x in enumerate(X):
     for j, t in enumerate(trees):
         distance, indices = t.query(x, k = 1, p = 2)
 
-        distance_matrix[i, j] += distance
-        distance_matrix[j, i] += distance
+        distances[(i, j)].append(distance)
 
-pickle.dump([sorted_keys, distance_matrix], open("distance_matrix.pickle", "wb"))
+pickle.dump([sorted_keys, distances], open("distances_%d.pickle" % FLAGS.shard, "wb"))
