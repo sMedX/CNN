3af931a71f3a1d335365bec3188f2c4155b918a1
Manvel Avetisian <avetisian@gmail.com>
2017-07-03 09:23:47 +0300

    Calculate distances to videos.

--- ultrasound/clusterize_videos.py
+++ ultrasound/clusterize_videos.py
@@ -4,6 +4,7 @@ import sys
 import gflags
 import numpy as np
 import pickle
+from datetime import datetime
 import sklearn.cluster
 import scipy.spatial
 
@@ -18,6 +19,9 @@ keys = list(vectors.keys())
 X = np.array(list(vectors.values()))
 print("%d keys, %s vectors loaded" % (len(keys), str(X.shape)))
 
+#X = X[0:1000]
+#keys = keys[0:1000]
+
 key_filenames = [filename_to_video(k) for k in keys]
 sorted_keys = sorted(set(key_filenames))
 key_indices = {v: k for k, v in enumerate(list(sorted_keys))}
@@ -30,15 +34,19 @@ trees = [scipy.spatial.KDTree(X[indexed_keys == k]) for k in range(len(key_indic
 
 distance_matrix = np.zeros((len(key_indices), len(key_indices)))
 
+start_time = datetime.now()
+
 for i, x in enumerate(X):
-    print(i)
+    time_passed = float((datetime.now() - start_time).total_seconds())
+    eta = (time_passed / (i + 1)) * (X.shape[0] - i) / 60 / 60
+    print("%8d\t%3.2f%%\teta = %.2f hours" % (i, 100. * i / float(X.shape[0]), eta))
+
+    i = indexed_keys[i]
 
     for j, t in enumerate(trees):
-        distances, indices = t.query(x, k = 20, p = 2)
+        distance, indices = t.query(x, k = 1, p = 2)
 
-        distance = np.mean(distances)
-
-        distance_matrix[i, j] = distance
-        distance_matrix[j, i] = distance
+        distance_matrix[i, j] += distance
+        distance_matrix[j, i] += distance
 
 pickle.dump([sorted_keys, distance_matrix], open("distance_matrix.pickle", "wb"))
